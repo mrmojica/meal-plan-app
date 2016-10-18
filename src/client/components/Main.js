e6f5d9fa8165c8cd7656065a77019e0457af1629
mrmojica <mrmojica510@gmail.com>
2016-10-18 12:00:40 -0700

    initial auth

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,14 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var WeekList = require('./WeekList');
-var Input = require('./UserInput');
+// var Input = require('./UserInput');
 
 
 var Main = function(){
 
 	return (
 		<div id="main">
-			<Input/>
 			<WeekList />
 		</div>
 
