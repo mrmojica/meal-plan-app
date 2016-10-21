24c25e6899937d405f8543f8ce479cb3400a19d2
mrmojica <mrmojica510@gmail.com>
2016-10-21 12:58:52 -0700

    fixed logout/now leaves google

--- src/client/components/WeekList.js
+++ src/client/components/WeekList.js
@@ -52,7 +52,7 @@ var WeekList = React.createClass({
 				</tbody>
 
 	 		</table>
-			<a href="/logout"><Button>Logout</Button></a>
+			<a href="https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:8080/"><Button>Logout</Button></a>
 		</div>
 
 		)
