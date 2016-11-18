9a3dddff7d1409b4708354ff7c34853f4c75b6d9
mrmojica <mrmojica510@gmail.com>
2016-11-18 09:31:49 -0800

    adj server-config

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/api' :
-                            'mongodb://localhost/api/meal-dev');
+                       (process.env.NODE_ENV = 'mongodb://meal-plan:mealplan123@ds157667.mlab.com:57667/meal-planner);
 exports.PORT = 8080 || process.env.PORT;
