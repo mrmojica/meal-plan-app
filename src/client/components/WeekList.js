28a98c46c0daeda8fef4eccb43ae53b5a6e71ddf
mrmojica <mrmojica510@gmail.com>
2016-11-21 10:43:42 -0800

    userInput error

--- src/client/components/WeekList.js
+++ src/client/components/WeekList.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var connect = require('react-redux').connect;
 var actions = require('../actions/actions');
-var Input = require('./UserInput');
+// var Input = require('./UserInput');
 var MealDetail = require ('./MealDetail');
 var Button = require('./Button');
 
