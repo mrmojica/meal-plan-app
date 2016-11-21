6e3713e3269a1c4b7732a8d84824879bb313f2bd
mrmojica <mrmojica510@gmail.com>
2016-11-21 10:35:48 -0800

    removing the port 8080 from the config.js

--- server/googleConfig.js
+++ server/googleConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '494705858866-nqkdasdjshg31l83id03a0qjako2ljmo.apps.googleusercontent.com',
         'clientSecret'  : 'n-o-KWZT2NExUghcc6k--mvH',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : '/auth/google/callback'
     }
 
 };
